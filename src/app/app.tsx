43c6ecc2b9cc33d21431aff34865c49edcc9e38f
Viktor Ivanov <vitochek337@gmail.com>
2024-08-12 11:36:47 +0300

    Initial prettier commit

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
-
-export function App() {
-  return (
-    <div>
-      <NxWelcome title="pixi-test" />
-    </div>
-  );
-}
+const App = () => {
+    return <div>App</div>;
+};
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,48 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 
 export default defineConfig({
-  root: __dirname,
-  cacheDir: './node_modules/.vite/pixi-test',
+    root: __dirname,
+    cacheDir: "./node_modules/.vite/pixi-test",
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  build: {
-    outDir: './dist/pixi-test',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    server: {
+        port: 4200,
+        host: "localhost",
     },
-  },
 
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './coverage/pixi-test',
-      provider: 'v8',
+    preview: {
+        port: 4300,
+        host: "localhost",
     },
-  },
+
+    plugins: [react(), nxViteTsPaths()],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+
+    build: {
+        outDir: "./dist/pixi-test",
+        emptyOutDir: true,
+        reportCompressedSize: true,
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
+
+    test: {
+        watch: false,
+        globals: true,
+        environment: "jsdom",
+        include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+
+        reporters: ["default"],
+        coverage: {
+            reportsDirectory: "./coverage/pixi-test",
+            provider: "v8",
+        },
+    },
 });
